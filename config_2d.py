d862e75d7b269890934eacd8d137484ddbc622aa
Mona Cui <37791691+Mona9955@users.noreply.github.com>
2022-09-09 18:46:08 +0800

    Add files via upload

---
+++ config_2d.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+LABEL_DICT = {'ApplyLipstick': 0, 'Rafting': 1, 'Skiing': 2, 'PoleVault': 3, 'LongJump': 4, 'PlayingDaf': 5, 'CricketBowling': 6, 'IceDancing': 7, 'JumpingJack': 8, 'WalkingWithDog': 9,
+              'FrisbeeCatch': 10, 'PlayingSitar': 11, 'ThrowDiscus': 12, 'Shotput': 13, 'SkateBoarding': 14, 'PlayingViolin': 15, 'ShavingBeard': 16, 'PlayingTabla': 17, 'Typing': 18,
+              'Punch': 19, 'PushUps': 20, 'PlayingGuitar': 21, 'VolleyballSpiking': 22, 'BalanceBeam': 23, 'FloorGymnastics': 24, 'Nunchucks': 25, 'PlayingFlute': 26, 'JugglingBalls': 27,
+              'TennisSwing': 28, 'Kayaking': 29, 'HorseRiding': 30, 'HulaHoop': 31, 'PullUps': 32, 'PlayingCello': 33, 'BoxingPunchingBag': 34, 'Rowing': 35, 'FieldHockeyPenalty': 36,
+              'WallPushups': 37, 'PlayingDhol': 38, 'MoppingFloor': 39, 'BandMarching': 40, 'Diving': 41, 'RopeClimbing': 42, 'BoxingSpeedBag': 43, 'GolfSwing': 44, 'Billiards': 45,
+              'HighJump': 46, 'Knitting': 47, 'WritingOnBoard': 48, 'CricketShot': 49, 'RockClimbingIndoor': 50, 'Bowling': 51, 'MilitaryParade': 52, 'ParallelBars': 53, 'JavelinThrow': 54,
+              'SumoWrestling': 55, 'HandstandPushups': 56, 'Mixing': 57, 'BasketballDunk': 58, 'PizzaTossing': 59, 'HorseRace': 60, 'SkyDiving': 61, 'SoccerPenalty': 62, 'TableTennisShot': 63,
+              'YoYo': 64, 'Hammering': 65, 'UnevenBars': 66, 'Basketball': 67, 'BreastStroke': 68, 'FrontCrawl': 69, 'Drumming': 70, 'Skijet': 71, 'BrushingTeeth': 72, 'SalsaSpin': 73,
+              'BenchPress': 74, 'TrampolineJumping': 75, 'HeadMassage': 76, 'Swing': 77, 'Lunges': 78, 'HandstandWalking': 79, 'JumpRope': 80, 'PommelHorse': 81, 'TaiChi': 82, 'CliffDiving': 83,
+              'StillRings': 84, 'SoccerJuggling': 85, 'Surfing': 86, 'CuttingInKitchen': 87, 'BodyWeightSquats': 88, 'Fencing': 89, 'BlowingCandles': 90, 'CleanAndJerk': 91, 'BaseballPitch': 92,
+              'PlayingPiano': 93, 'BabyCrawling': 94, 'Archery': 95, 'Haircut': 96, 'HammerThrow': 97, 'BlowDryHair': 98, 'ApplyEyeMakeup': 99, 'Biking': 100}
+LABEL_DICT_11 = {'Basketball':0, 'Biking':1, 'Diving':2, 'GolfSwing':3, 'HorseRiding':4, 'SoccerJuggling':5, 'Swing':6, 'TennisSwing':7, 'TrampolineJumping':8, 'VolleyballSpiking':9, 'WalkingWithDog':10}
+LABEL_DICT_50 = {'BaseballPitch': 0, 'Basketball': 1, 'BenchPress': 2, 'Biking': 3, 'Billiards': 4, 'BreastStroke': 5, 'CleanAndJerk': 6, 'Diving': 7, 'Drumming': 8, 'Fencing': 9, 'GolfSwing': 10, 
+'HighJump': 11, 'HorseRace': 12, 'HorseRiding': 13, 'HulaHoop': 14, 'JavelinThrow': 15, 'JugglingBalls': 16, 'JumpingJack': 17, 'JumpRope': 18, 'Kayaking': 19, 'Lunges': 20, 'MilitaryParade': 21, 
+'Mixing': 22, 'Nunchucks': 23, 'PizzaTossing': 24, 'PlayingGuitar': 25, 'PlayingPiano': 26, 'PlayingTabla': 27, 'PlayingViolin': 28, 'PoleVault': 29, 'PommelHorse': 30, 'PullUps': 31, 'Punch': 32, 
+'PushUps': 33, 'RockClimbingIndoor': 34, 'RopeClimbing': 35, 'Rowing': 36, 'SalsaSpin': 37, 'SkateBoarding': 38, 'Skiing': 39, 'Skijet': 40, 'SoccerJuggling': 41, 'Swing': 42, 'TaiChi': 43, 
+'TennisSwing': 44, 'ThrowDiscus': 45, 'TrampolineJumping': 46, 'VolleyballSpiking': 47, 'WalkingWithDog': 48, 'YoYo': 49}
+LABEL_DICT_JHMDB = {'brush_hair': 0, 'catch': 1, 'clap': 2, 'climb_stairs': 3, 'golf': 4, 'jump': 5, 'kick_ball': 6, 'pick': 7, 'pour': 8, 'pullup': 9, 'push': 10,
+              'run': 11, 'shoot_ball': 12, 'shoot_bow': 13, 'shoot_gun': 14, 'sit': 15, 'stand': 16, 'swing_baseball': 17, 'throw': 18, 'walk': 19, 'wave': 20}
+
+LABEL_DICT_HMDB51 = {'draw_sword': 0, 'eat': 1, 'laugh': 2, 'run': 3, 'brush_hair': 4, 'hit': 5, 'shoot_gun': 6, 'sword_exercise': 7, 'chew': 8, 'drink': 9, 'pushup': 10, 'shoot_bow': 11, 'wave': 12,
+ 'situp': 13, 'shoot_ball': 14, 'turn': 15, 'hug': 16, 'shake_hands': 17, 'golf': 18, 'smoke': 19, 'handstand': 20, 'sword': 21, 'sit': 22, 'smile': 23, 'catch': 24, 'fencing': 25,
+ 'ride_bike': 26, 'throw': 27, 'pick': 28, 'somersault': 29, 'ride_horse': 30, 'flic_flac': 31, 'punch': 32, 'dive': 33, 'kick': 34, 'cartwheel': 35, 'kiss': 36, 'kick_ball': 37,
+ 'fall_floor': 38, 'walk': 39, 'push': 40, 'swing_baseball': 41, 'climb_stairs': 42, 'stand': 43, 'clap': 44, 'pullup': 45, 'pour': 46, 'climb': 47, 'jump': 48, 'talk': 49, 'dribble': 50}
+
+LABEL_DICT_KTH = {'boxing':0, 'handclapping':1, 'handwaving':2, 'jogging':3, 'running':4, 'walking':5}
+LABEL_DICT_PANN = {'baseball_pitch': 0, 'baseball_swing': 1, 'bench_press': 2, 'bowl': 3, 'clean_and_jerk': 4, 'golf_swing': 5, 'jumping_jacks': 6, 'jump_rope': 7, 'pullup': 8, 'pushup': 9,
+                   'situp': 10, 'squat': 11, 'strum_guitar': 12, 'tennis_forehand': 13, 'tennis_serve': 14}
+
+LABEL_DICT_NTU = {'drink_water': 0, 'eat_meal/snack': 1, 'brushing_teeth': 2, 'brushing_hair': 3, 'drop': 4, 'pickup': 5, 'throw': 6, 'sitting_down': 7, 'standing_up_(from_sitting_position)': 8,
+                  'clapping': 9, 'reading': 10, 'writing': 11, 'tear_up_paper': 12, 'wear_jacket': 13, 'take_off_jacket': 14, 'wear_a_shoe': 15, 'take_off_a_shoe': 16, 'wear_on_glasses': 17,
+                  'take_off_glasses': 18, 'put_on_a_hat/cap': 19, 'take_off_a_hat/cap': 20, 'cheer_up': 21, 'hand_waving': 22, 'kicking_something': 23, 'reach_into_pocket': 24,
+                  'hopping_(one_foot_jumping)': 25, 'jump_up': 26, 'make_a_phone_call/answer_phone': 27, 'playing_with_phone/tablet': 28, 'typing_on_a_keyboard': 29,
+                  'pointing_to_something_with_finger': 30, 'taking_a_selfie': 31, 'check_time_(from_watch)': 32, 'rub_two_hands_together': 33, 'nod_head/bow': 34, 'shake_head': 35,
+                  'wipe_face': 36, 'salute': 37, 'put_the_palms_together': 38, 'cross_hands_in_front_(say_stop)': 39, 'sneeze/cough': 40, 'staggering': 41, 'falling': 42,
+                  'touch_head_(headache)': 43, 'touch_chest_(stomachache/heart_pain)': 44, 'touch_back_(backache)': 45, 'touch_neck_(neckache)': 46, 'nausea_or_vomiting_condition': 47,
+                  'use_a_fan_(with_hand_or_paper)/feeling_warm': 48, 'punching/slapping_other_person': 49, 'kicking_other_person': 50, 'pushing_other_person': 51, 'pat_on_back_of_other_person': 52,
+                  'point_finger_at_the_other_person': 53, 'hugging_other_person': 54, 'giving_something_to_other_person': 55, "touch_other_person's_pocket": 56, 'handshaking': 57, 'walking_towards_each_other': 58, 'walking_apart_from_each_other': 59}
+
+BATCH_SIZE = 4 #4
+
+BATCH_SIZE_TEST = 4  #4
+
+GPUS = [0,1]
+
+IMG_SIZE = 224
+
+MAX_FRAMES = 16
+
+MAX_IMG_SIZE = 256
+
+IMG_MEAN = [104, 117, 128]
+
+IMG_STD = [1]
+
+IMG_SCALE = [1, .875, .75, .66] #[1, .875, .75]  [1, .875, .75, .66]
+
+# NUM_CLASSES = len(LABEL_DICT)
+
+NUM_EPOCHS = 2000
+
+REGULARIZATION_RATE = 4e-5
+
+LR_RATE = 1e-3  #1e-4
+
+WARM_UP_STEPS = 200
+
+MODEL_DIR = './checkpoint_ntu'
+
+PRETRAINED_DIR = "/home/mengmeng/project/PAV/inception_v2.ckpt"  #'./inception_v2.ckpt'
+
+CKPT_DIR = ''
+
+PERIOD = 200
+
+SAVE_PERIOD = PERIOD
+
+TEST_PERIOD = PERIOD
+
+SUMMARY_PERIOD = PERIOD
+
+MAX_PEOPLE = 2  #4
+
+MAX_POINTS = 14
+
+DECODER_SIZE = 512
+
+LSTM_SIZE = 512
+
+THREAD_NUM = 4
+
+USE_MT = True
+
+Flip = 0.7
